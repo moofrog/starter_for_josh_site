19428ae7956f0f737883faa582315c831a90bc2c
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-10 22:03:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+      <section class="container-fluid aboutbg text-center"> <!--have to have a heading in this otherwise it wont valiate? -->
+          <div class="container">
+              <h2>GAMES &amp; LEVEL DESIGNER</h2>
+              <p class="particle">...and particle wizard</p>
+              <p class="about-text">My  name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+              
+                <div class="blocker"></div>
+              
+          </div>
+      </section>
+      
+       <!-- End of container-->
+       <!-- ctrl + ? = shortcut for commenting out things  -->
+
+      <section class="container-fluid articlesbg"> <!-- main image can stretch to edge of page -->
+          <div class="container"> <!-- holds content in middle of page -->
+              <div class="row"> <!-- Gives access to all the bootstrap columns (the blue -->                                       <!-- lines you can see on the photoshop file for josh hw)-->
+                  <div class="text-center article-title">
+                    <h3>LATEST ARTICLES</h3>
+                  </div>
+                  
+                  <div class="col-md-4 article-cent"> <!-- column, medium size, 4 columns/rows? wide-->
+                      <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/portrait.jpg">
+                      <h4>A PORTRAIT</h4>
+                      <p class="date">12 December 2020</p>
+                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
+                      <p class="readmore">CONTINUE READING</p>
+                  </div>
+                  
+                  <div class="col-md-4 article-cent">
+                      <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
+                      <h4>A MODEL</h4>
+                      <p class="date">12 December 2020</p>
+                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
+                      <p class="readmore">CONTINUE READING</p>
+                  </div>
+                  
+                  <div class="col-md-4 article-cent">
+                      <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
+                      <h4>A GAMES DESIGN</h4>
+                      <p class="date">12 December 2020</p>
+                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
+                      <p class="readmore">CONTINUE READING</p>
+                  </div>
+                  
+              </div> <!-- end row -->
+          </div> <!-- end container -->
+      </section><!-- end container-fluid -->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+      

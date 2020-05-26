8ec49a76c065952fa73de82415cd40c41b6c91dc
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-26 16:08:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-      
       <section class="container-fluid aboutbg text-center"> <!--have to have a heading in this otherwise it wont valiate? -->
           <div class="container">
               <h2>GAMES &amp; LEVEL DESIGNER</h2>
               <p class="particle">...and particle wizard</p>
               <p class="about-text">My  name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
               
-                <div class="blocker"></div>
+              <div class="blocker"></div>
               
           </div>
       </section>

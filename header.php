992e3e655a0b11b35d13bb3e5b6bc030169cc2d7
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-26 14:00:08 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="<?php bloginfo("charset"); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta charset="<?php bloginfo("charset"); ?>"> <!-- looks inside wordpress theme for charset -->
       
-      <link rel="shortcut icon" href="<?php bloginfo("template_directory"); ?>/_/img/favicon.ica">
       
-    <title> 
+    <title> <!-- automatically generated in wordpress -->
         <?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -26,25 +22,22 @@
 		          bloginfo('name'); }
 		      if ($paged>1) {
 		         echo ' - page '. $paged; }
-		   ?>
-
-      
-      
+		 ?>
     </title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta name="description" content="<?php bloginfo('description'); ?>">  
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="responsive.css"/>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">  
-    <link rel="stylesheet" href="style.css"/><!-- should always be at bottom -->
-<?php wp_head(); ?> <!--very important that this is added. tells wordpress this is header. cant be any spaces before it -->
+<!-- viewport responsiveness -->      
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- fabicon -->  
+    <link rel="shortcut icon" href="<?php bloginfo("template_directory"); ?>/_/img/favicon.ica">
+      
+<!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
+
+<?php wp_head(); ?><!--'Hook'. very important that this is added. tells wordpress this is header. cant be any spaces before it -->
+
       
   </head>
   
@@ -94,7 +87,3 @@
             <h1>Games Design For the Future</h1>  
         </div>
     </header><!-- no spaces after this cause it will break php>
-      
-      
-  </body>
-</html>

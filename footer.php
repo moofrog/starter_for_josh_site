44cbb95448b1128a042bd5ee13fd35efe9a6409d
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-26 14:00:04 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,23 @@
 <footer class="container-fluid">
     <div class="container">
         <div class="row">
+
             <div class="col-md-6 text-center footer-content">
 <p> <?php bloginfo('name'); ?></p>
-                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
-                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
+                <img src="http://206.189.45.97/~mesh20/sub2/wordpress_content/themes/starter_for_josh_site/images/articles-img1.jpg" alt=“tumblr” >
+                <img src="http://206.189.45.97/~mesh20/sub2/wordpress_content/themes/starter_for_josh_site/images/articles-img1.jpg" alt=“rss” >
             </div>
+            
             <div class="col-md-6 text-center footer-content">
                 <p>Terms and Conditions</p>
-                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin” >
-                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt=“facebook” >
+                <img src="http://206.189.45.97/~mesh20/sub2/wordpress_content/themes/starter_for_josh_site/images/articles-img1.jpg" alt=“linkedin” >
+                <img src="http://206.189.45.97/~mesh20/sub2/wordpress_content/themes/starter_for_josh_site/images/articles-img1.jpg" alt=“facebook” >
             </div>
 
         </div>
+        <!-- row-->
     </div>
+    <!-- container-->
 </footer>
 </body>
 </html>

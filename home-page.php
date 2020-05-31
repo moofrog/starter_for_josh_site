62d117b4726401359bc2c337285e04315a3b5697
moofrog <60612224+moofrog@users.noreply.github.com>
2020-06-01 00:32:30 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -23,24 +23,35 @@ Template Name: Home page
           <div class="container"> <!-- holds content in middle of page -->
               <div class="row"> <!-- Gives access to all the bootstrap columns (the blue -->                                       <!-- lines you can see on the photoshop file for josh hw)-->
                   <div>
-                    <h3 class="text-center article-title wow bounceInUp"><?php the_field('titleforarticles'); ?></h3>
+                    <h3 class="text-center article-title"><?php the_field('titleforarticles'); ?></h3>
                   </div>
                   
                   <div class="col-md-4 article-cent"> <!-- column, medium size, 4 columns/rows? wide-->
 
         <!--placeholder for col1-->              
+    <img class="img-responsive" alt="portrait" src="<?php the_field('articleimage'); ?>">
+    <h4><?php the_field('articletitle'); ?></h4>
+    <p><?php the_field('articleexcerpt');?></p>
+    <a href="<?php the_field('readmore');?>" class="readmore">CONTINUE READING</a>
+                  </div>
+                  
+                  <div class="col-md-4 article-cent">
+
+       <!--placeholder for col1-->  
+     <img class="img-responsive" alt="model" src="<?php the_field('articletwoimage'); ?>">
+    <h4><?php the_field('articletwotitle'); ?></h4>
+    <p><?php the_field('articletwoexcerpt');?></p>
+    <a href="<?php the_field('readmore');?>" class="readmore">CONTINUE READING</a>                  
                       
-
                   </div>
                   
                   <div class="col-md-4 article-cent">
 
        <!--placeholder for col1-->               
-                  </div>
-                  
-                  <div class="col-md-4 article-cent">
-
-       <!--placeholder for col1-->               
+    <img class="img-responsive" alt="games design" src="<?php the_field('articlethreeimage'); ?>">
+    <h4><?php the_field('articlethreetitle'); ?></h4>
+    <p><?php the_field('articlethreeexcerpt');?></p>
+    <a href="<?php the_field('readmore');?>" class="readmore">CONTINUE READING</a>
                       
                   </div>
                   

fb1a60ecaea42ca2adc82929727c7f86b176ccb2
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-31 21:14:31 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
-<?php  <!-- no space before -->
+<?php  // no space before
 function theme_add_bootstrap() {
 
-<!-- css elements -->    
+//css elements   
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
     
-<!--javascript elements-->
+//javascript elements
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
 }
  
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
-<!--no spaces after-->
+//no spaces after
 ?>

d2a3e7e316c66f4256913c914cd106a4c9ab1c25
moofrog <60612224+moofrog@users.noreply.github.com>
2020-05-31 23:59:25 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,6 +8,8 @@ Template Name: Home page
           <div class="container">
 
 <!--placeholder for aboutme-->
+ <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->           <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>  
+ <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p> 
               
               <div class="blocker"></div>
               
